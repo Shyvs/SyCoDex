2b643eeafca842dc060ca7bac972aac7ad14cd86
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-22 22:01:37 -0500

    Add functions for seed generation and seeded random

--- js/random.js
+++ js/random.js
@@ -1,1 +1,21 @@
+function createSeed(name) {
 
+    let seed = 0;
+
+    for(let i = 0; i < name.length; i++) {
+
+        seed += name.charCodeAt(i);
+
+    }
+
+    return seed;
+}
+
+
+function seededRandom(seed) {
+
+    let x = Math.sin(seed) * 10000;
+
+    return x - Math.floor(x);
+
+}
