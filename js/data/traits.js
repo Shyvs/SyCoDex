5d4b779462bc1332c5a8438605370ccd6dcd7f25
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-22 22:45:54 -0500

    Refactor creature definitions in traits.js

--- js/data/traits.js
+++ js/data/traits.js
@@ -1,101 +1,193 @@
 const shapes = [
-  {
-        name: "fox-like creature",
-        types: ["Fire", "Fairy", "Dark", "Normal"]
-    },
+{
+    name: "fox-like creature",
+    types: ["Fire", "Fairy", "Dark", "Normal"]
+},
+  
+{
+    name: "round and fluffy creature",
+    types: ["Normal", "Fairy", "Ice"]
+},
+  
+{
+    name: "long serpentine creature",
+    types: ["Dragon", "Water", "Poison"]
+},
+  
+{
+    name: "insect-like creature",
+    types: ["Bug", "Poison", "Grass"]
+},
 
-    {
-        name: "round and fluffy creature",
-        types: ["Normal", "Fairy", "Ice"]
-    },
+{
+    name: "four-legged beast",
+    types: ["Normal", "Ground", "Fighting"]
+},
+  
+{
+    name: "floating creature",
+    types: ["Ghost", "Psychic", "Flying"]
+},
+  
+{
+    name: "bird-like creature",
+    types: ["Flying", "Normal", "Electric"]
+},
+  
+{
+    name: "amphibious creature",
+    types: ["Water", "Poison", "Grass"]
+},
+  
+{
+    name: "humanoid creature",
+    types: ["Psychic", "Fighting", "Fairy"]
+},
+  
+{
+    name: "mechanical creature",
+    types: ["Steel", "Electric"]
+},
+  
+{
+    name: "mushroom-like creature",
+    types: ["Grass", "Poison", "Fairy"]
+},
+  
+{
+    name: "crystalline creature",
+    types: ["Rock", "Ice", "Fairy"]
+},
+  
+{
+    name: "shadowy creature",
+    types: ["Dark", "Ghost"]
+},
+  
+{
+    name: "celestial creature",
+    types: ["Psychic", "Fairy", "Dragon"]
+},
+  
+{
+    name: "rodent-like creature",
+    types: ["Normal", "Electric", "Dark", "Grass"]
+},
 
-    {
-        name: "long serpentine creature",
-        types: ["Dragon", "Water", "Poison"]
-    },
+{
+    name: "bear-like creature",
+    types: ["Normal", "Fighting", "Ice", "Ground"]
+},
 
- {
-        name: "insect-like creature",
-        types: ["Bug", "Poison", "Grass"]
-    },
+{
+    name: "cat-like creature",
+    types: ["Normal", "Dark", "Psychic", "Fairy"]
+},
 
-    {
-        name: "four-legged beast",
-        types: ["Normal", "Ground", "Fighting"]
-    },
+{
+    name: "wolf-like creature",
+    types: ["Dark", "Ice", "Normal", "Fighting"]
+},
 
-    {
-        name: "floating creature",
-        types: ["Ghost", "Psychic", "Flying"]
-    },
+{
+    name: "deer-like creature",
+    types: ["Grass", "Fairy", "Normal", "Psychic"]
+},
 
-    {
-        name: "bird-like creature",
-        types: ["Flying", "Normal", "Electric"]
-    },
+{
+    name: "reptilian creature",
+    types: ["Dragon", "Poison", "Ground", "Fire"]
+},
 
-    {
-        name: "amphibious creature",
-        types: ["Water", "Poison", "Grass"]
-    },
+{
+    name: "turtle-like creature",
+    types: ["Water", "Rock", "Ground", "Steel"]
+},
 
-    {
-        name: "humanoid creature",
-        types: ["Psychic", "Fighting", "Fairy"]
-    },
+{
+    name: "frog-like creature",
+    types: ["Water", "Poison", "Grass", "Bug"]
+},
 
-    {
-        name: "mechanical creature",
-        types: ["Steel", "Electric"]
-    },
- {
-        name: "mushroom-like creature",
-        types: ["Grass", "Poison", "Fairy"]
-    },
+{
+    name: "moth-like creature",
+    types: ["Bug", "Flying", "Psychic", "Dark"]
+},
 
-    {
-        name: "crystalline creature",
-        types: ["Rock", "Ice", "Fairy"]
-    },
+{
+    name: "butterfly-like creature",
+    types: ["Bug", "Flying", "Fairy", "Psychic"]
+},
 
-    {
-        name: "shadowy creature",
-        types: ["Dark", "Ghost"]
-    },
+{
+    name: "bat-like creature",
+    types: ["Flying", "Dark", "Poison", "Ghost"]
+},
 
-    {
-        name: "celestial creature",
-        types: ["Psychic", "Fairy", "Dragon"]
-    }
+{
+    name: "dragon-like creature",
+    types: ["Dragon", "Fire", "Flying", "Water"]
+},
 
-    "rodent-like creature",
-    "bear-like creature",
-    "cat-like creature",
-    "wolf-like creature",
-    "deer-like creature",
-    "reptilian creature",
-    "turtle-like creature",
-    "frog-like creature",
-    "moth-like creature",
-    "butterfly-like creature",
-    "bat-like creature",
-    "dragon-like creature",
-    "plant-like creature",
-   
-    "blob-like creature",
-    "jelly-like creature",
-    "crab-like creature",
-    "spider-like creature",
-    "fish-like creature",
-    "shark-like creature",
-    "fairy-like creature",
-    "armored creature",
-    "ghostly apparition",
+{
+    name: "plant-like creature",
+    types: ["Grass", "Fairy", "Poison", "Bug"]
+},
 
+{
+    name: "blob-like creature",
+    types: ["Normal", "Poison", "Water", "Ghost"]
+},
 
-    "cloud-like creature",
+{
+    name: "jelly-like creature",
+    types: ["Water", "Poison", "Ghost", "Ice"]
+},
 
-    "energy-based creature"
+{
+    name: "crab-like creature",
+    types: ["Water", "Rock", "Fighting", "Steel"]
+},
+
+{
+    name: "spider-like creature",
+    types: ["Bug", "Poison", "Dark", "Electric"]
+},
+
+{
+    name: "fish-like creature",
+    types: ["Water", "Ice", "Dragon", "Normal"]
+},
+
+{
+    name: "shark-like creature",
+    types: ["Water", "Dark", "Dragon", "Ground"]
+},
+
+{
+    name: "fairy-like creature",
+    types: ["Fairy", "Psychic", "Flying", "Grass"]
+},
+
+{
+    name: "armored creature",
+    types: ["Steel", "Rock", "Ground", "Bug"]
+},
+
+{
+    name: "ghostly apparition",
+    types: ["Ghost", "Dark", "Psychic"]
+},
+
+{
+    name: "cloud-like creature",
+    types: ["Flying", "Electric", "Ice", "Fairy"]
+},
+
+{
+    name: "energy-based creature",
+    types: ["Electric", "Psychic", "Fire", "Ghost"]
+}
 ];
 
 
