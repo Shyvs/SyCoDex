ffc7fe0547f870c35863b2281ad0a2a351d97924
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-22 23:23:58 -0500

    Implement pickFromList function for seeded selection
    Added pickFromList function to select an item from a list using a seed for randomness.

--- js/random.js
+++ js/random.js
@@ -3,12 +3,11 @@ function createSeed(name) {
     let seed = 0;
 
     for(let i = 0; i < name.length; i++) {
-
         seed += name.charCodeAt(i);
-
     }
 
     return seed;
+
 }
 
 
@@ -17,5 +16,16 @@ function seededRandom(seed) {
     let x = Math.sin(seed) * 10000;
 
     return x - Math.floor(x);
+
+}
+
+
+function pickFromList(list, seed) {
+
+    return list[
+        Math.floor(
+            seededRandom(seed) * list.length
+        )
+    ];
 
 }
