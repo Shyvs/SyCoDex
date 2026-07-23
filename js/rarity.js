4c12d94a89b66bd0806700fc2ddc25542900dada
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-22 23:25:36 -0500

    Add calculateRarity function to classify scores

--- js/rarity.js
+++ js/rarity.js
@@ -1,1 +1,20 @@
+function calculateRarity(score) {
 
+
+    if(score === 4) {
+
+        return "Common";
+
+    }
+
+
+    if(score >= 2) {
+
+        return "Uncommon";
+
+    }
+
+
+    return "Rare";
+
+}
