e848226f3d9a94c17d1fc2f99c36a14d35864b6d
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-24 20:26:46 -0500

    Update flavor.js

--- js/data/flavor.js
+++ js/data/flavor.js
@@ -26,18 +26,23 @@ const habitats = [
 ];
 
 
-
 const personalities = [
-    "Curious and playful.",
-    "Quiet and mysterious.",
-    "Friendly and loyal.",
-    "Mischievous and clever.",
-    "Protective and brave.",
-    "Shy but curious.",
-    "Playful and energetic.",
-    "Calm and protective.",
-    "Serious and independent.",
-    "Gentle and nurturing."
+
+    ["Cowardly", "Brave", "Bold", "Timid"],
+    ["Lazy", "Energetic"],
+    ["Quiet", "Loud"],
+    ["Serious", "Mischievous", "Goofy", "Playful"],
+    ["Aloof", "Friendly", "Outgoing", "Reserved", "Solitary"],
+    ["Independent", "Loyal"],
+    ["Indifferent", "Curious"],
+    ["Protective", "Carefree"],
+    ["Mysterious", "Expressive"],
+    ["Nurturing", "Competitive"],
+    ["Clever", "Clumsy"],
+    ["Aggressive", "Calm"],
+    ["Optimistic", "Pessimistic"],
+    ["Patient", "Impulsive"]
+
 ];
 
 
