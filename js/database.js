9fa3d2411a80e7e60cb7b60b3c7f1280e537aa31
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-07-24 22:49:10 -0500

    Implement findSyCoMon and saveDiscovery functions
    Added functions to find and save SyCoMon.

---
+++ js/database.js
@@ -0,0 +1,16 @@
+function findSyCoMon(name) {
+
+    // Later:
+    // Search Supabase here
+
+    return null;
+
+}
+
+
+function saveDiscovery(sycomon) {
+
+    // Later:
+    // Send discovered SyCoMon to Supabase
+
+}
