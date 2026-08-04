5c2274803947b1b3b3190804830df08b8902cd37
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-03 21:34:35 -0500

    Refactor shapes to body array with creature names

---
+++ js/data/traits/body.js
@@ -0,0 +1,133 @@
+const body = [
+
+{
+    name: "fox",
+    types: ["Fire", "Fairy", "Dark", "Normal"]
+},
+
+{
+    name: "rodent",
+    types: ["Normal", "Electric", "Dark", "Grass"]
+},
+
+{
+    name: "cat",
+    types: ["Normal", "Dark", "Psychic", "Fairy"]
+},
+
+{
+    name: "wolf",
+    types: ["Dark", "Ice", "Normal", "Fighting"]
+},
+
+{
+    name: "bear",
+    types: ["Normal", "Fighting", "Ice", "Ground"]
+},
+
+{
+    name: "deer",
+    types: ["Grass", "Fairy", "Normal", "Psychic"]
+},
+
+{
+    name: "dog",
+    types: ["Normal", "Fighting", "Fairy", "Ground"]
+},
+
+{
+    name: "bird",
+    types: ["Flying", "Normal", "Electric"]
+},
+
+{
+    name: "bat",
+    types: ["Flying", "Dark", "Poison", "Ghost"]
+},
+
+{
+    name: "fish",
+    types: ["Water", "Ice", "Dragon", "Normal"]
+},
+
+{
+    name: "shark",
+    types: ["Water", "Dark", "Dragon", "Ground"]
+},
+
+{
+    name: "frog",
+    types: ["Water", "Poison", "Grass", "Bug"]
+},
+
+{
+    name: "turtle",
+    types: ["Water", "Rock", "Ground", "Steel"]
+},
+
+{
+    name: "crab",
+    types: ["Water", "Rock", "Fighting", "Steel"]
+},
+
+{
+    name: "spider",
+    types: ["Bug", "Poison", "Dark", "Electric"]
+},
+
+{
+    name: "insect",
+    types: ["Bug", "Poison", "Grass"]
+},
+
+{
+    name: "moth",
+    types: ["Bug", "Flying", "Psychic", "Dark"]
+},
+
+{
+    name: "butterfly",
+    types: ["Bug", "Flying", "Fairy", "Psychic"]
+},
+
+{
+    name: "serpent",
+    types: ["Dragon", "Water", "Poison"]
+},
+
+{
+    name: "dragon",
+    types: ["Dragon", "Fire", "Flying", "Water"]
+},
+
+{
+    name: "reptile",
+    types: ["Dragon", "Poison", "Ground", "Fire"]
+},
+
+{
+    name: "plant",
+    types: ["Grass", "Fairy", "Poison", "Bug"]
+},
+
+{
+    name: "mushroom",
+    types: ["Grass", "Poison", "Fairy"]
+},
+
+{
+    name: "humanoid",
+    types: ["Psychic", "Fighting", "Fairy"]
+},
+
+{
+    name: "blob",
+    types: ["Normal", "Poison", "Water", "Ghost"]
+},
+
+{
+    name: "jelly",
+    types: ["Water", "Poison", "Ghost", "Ice"]
+}
+
+];
