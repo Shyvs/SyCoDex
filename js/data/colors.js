c3ee123d61274cb1796792fed3663cc71c547bb7
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 11:16:30 -0500

    Create colors.js with color definitions
    Added a colors array with color names and modifiers.

---
+++ js/data/colors.js
@@ -0,0 +1,59 @@
+const colors = [
+    {
+        name: "red",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "orange",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "yellow",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "green",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "blue",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "purple",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "pink",
+        modifiers: ["", "light", "dark", "vibrant", "muted"]
+    },
+    {
+        name: "brown",
+        modifiers: ["", "light", "dark", "rich", "muddy"]
+    },
+    {
+        name: "gray",
+        modifiers: ["", "light", "dark"]
+    },
+    {
+        name: "black",
+        modifiers: ["", "jet"]
+    },
+    {
+        name: "white",
+        modifiers: ["", "pure"]
+    },
+    {
+        name: "silver",
+        modifiers: ["", "shining"]
+    },
+
+    {
+        name: "gold",
+        modifiers: ["", "glistening"]
+    },
+    {
+        name: "bronze",
+        modifiers: [""]
+    }
+];
