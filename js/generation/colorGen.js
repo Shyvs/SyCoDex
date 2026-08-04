8dbb4a0df75a0bee27385c493357367f8801273a
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 09:52:17 -0500

    Refactor color array by removing extra line breaks
    Removed unnecessary line breaks between color objects for cleaner code.

--- js/generation/colorGen.js
+++ js/generation/colorGen.js
@@ -1,60 +1,48 @@
 const colors = [
-
     {
         name: "red",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "orange",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "yellow",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "green",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "blue",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "purple",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "pink",
         modifiers: ["", "light", "dark", "vibrant", "muted"]
     },
-
     {
         name: "brown",
         modifiers: ["", "light", "dark", "rich", "muddy"]
     },
-
     {
         name: "gray",
         modifiers: ["", "light", "dark"]
     },
-
     {
         name: "black",
         modifiers: ["", "jet"]
     },
-
     {
         name: "white",
         modifiers: ["", "pure"]
     },
-
     {
         name: "silver",
         modifiers: ["", "shining"]
@@ -64,14 +52,11 @@ const colors = [
         name: "gold",
         modifiers: ["", "glistening"]
     },
-
     {
         name: "bronze",
         modifiers: [""]
     }
-
 ];
-
 
 function generateColors(seed) {
 
