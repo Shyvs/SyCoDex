ea7c8429404f5a543b70e3e87063b76162b9a39b
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 09:51:14 -0500

    Add personality traits for generation
    Added a list of personality traits categorized for use in personality generation.

---
+++ js/generation/personalityGen.js
@@ -0,0 +1,40 @@
+const personalities = [
+
+    ["Cowardly", "Brave", "Bold", "Timid"],
+    ["Lazy", "Energetic"],
+    ["Quiet", "Loud"],
+    ["Serious", "Mischievous", "Goofy", "Playful"],
+    ["Aloof", "Friendly", "Outgoing", "Reserved", "Solitary"],
+    ["Independent", "Loyal"],
+    ["Indifferent", "Curious"],
+    ["Protective", "Carefree"],
+    ["Mysterious", "Expressive"],
+    ["Nurturing", "Competitive"],
+    ["Clever", "Clumsy"],
+    ["Aggressive", "Calm"],
+    ["Optimistic", "Pessimistic"],
+    ["Patient", "Impulsive"]
+
+];
+
+function generatePersonality(seed) {
+
+    let category1 =
+        pickFromList(personalities, seed);
+
+    let category2 =
+        pickDifferent(
+            s => pickFromList(personalities, s),
+            seed + 20,
+            category1
+        );
+
+    let personality1 =
+        pickFromList(category1, seed + 1);
+
+    let personality2 =
+        pickFromList(category2, seed + 2);
+
+    return `${personality1} and ${personality2}.`;
+
+}
