8b3f1ba8cbbccf5ebce0c2dcdec09581f876609b
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 17:33:31 -0500

    Remove calculateRarity function from rarity.js
    Removed the calculateRarity function and its logic.

--- js/util/rarity.js
+++ js/util/rarity.js
@@ -15,7 +15,6 @@ function calculateRarity(seed) {
     if (roll < 2) return "Legendary";
     if (roll < 20) return "Rare";
     if (roll < 50) return "Uncommon";
-
     return "Common";
 
 }
@@ -24,35 +23,3 @@ function calculateRarity(seed) {
 //0.50 - 0.80  Uncommon   30%
 //0.80 - 0.98  Rare       18%
 //0.98 - 1.00  Legendary   2%
-
-function calculateRarity(seed) {
-
-    let roll =
-        seededRandom(seed);
-
-
-    if (roll < 0.50) {
-
-        return "Common";
-
-    }
-
-    else if (roll < 0.80) {
-
-        return "Uncommon";
-
-    }
-
-    else if (roll < 0.98) {
-
-        return "Rare";
-
-    }
-
-    else {
-
-        return "Legendary";
-
-    }
-
-}
