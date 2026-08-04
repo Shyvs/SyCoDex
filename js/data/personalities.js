ca3543bbca15dbbcb16dc499ef8d8c5ca5c886d2
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 11:14:15 -0500

    Add personalityGroups array to personalities.js

---
+++ js/data/personalities.js
@@ -0,0 +1,18 @@
+const personalityGroups = [
+
+    ["Cowardly", "Brave", "Bold", "Timid"],
+    ["Lazy", "Energetic"],
+    ["Quiet", "Loud"],
+    ["Serious", "Mischievous", "Goofy", "Playful"],
+    ["Aloof", "Friendly", "Outgoing", "Reserved", "Solitary"],
+    ["Independent", "Loyal"],
+    ["Indifferent", "Curious"],
+    ["Protective", "Carefree"],
+    ["Mysterious", "Expressive"],
+    ["Nurturing", "Competitive"],
+    ["Clever", "Clumsy"],
+    ["Aggressive", "Calm"],
+    ["Optimistic", "Pessimistic"],
+    ["Patient", "Impulsive"]
+
+];
