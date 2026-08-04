99490d15ba548feed7e01f0d658bcd7d2ac37819
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 11:14:32 -0500

    Remove personalityGroups array
    Removed the personalityGroups array from personalityGen.js.

--- js/generation/personalityGen.js
+++ js/generation/personalityGen.js
@@ -1,22 +1,3 @@
-const personalityGroups = [
-
-    ["Cowardly", "Brave", "Bold", "Timid"],
-    ["Lazy", "Energetic"],
-    ["Quiet", "Loud"],
-    ["Serious", "Mischievous", "Goofy", "Playful"],
-    ["Aloof", "Friendly", "Outgoing", "Reserved", "Solitary"],
-    ["Independent", "Loyal"],
-    ["Indifferent", "Curious"],
-    ["Protective", "Carefree"],
-    ["Mysterious", "Expressive"],
-    ["Nurturing", "Competitive"],
-    ["Clever", "Clumsy"],
-    ["Aggressive", "Calm"],
-    ["Optimistic", "Pessimistic"],
-    ["Patient", "Impulsive"]
-
-];
-
 function generatePersonality(seed) {
 
     let category1 =
