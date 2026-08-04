70b6a568e8b1b5f6c9eb8abafa0940511954f69b
Shyvs <50509302+Shyvs@users.noreply.github.com>
2026-08-04 15:46:54 -0500

    Update and rename colors.js to colorPalettes.js

---
+++ js/data/colorPalettes.js
@@ -0,0 +1,222 @@
+const colorPalettes = [
+
+    // Bulbasaur
+    {
+        types: ["Grass", "Poison"],
+        colors: [
+            "#399494",
+            "#62d5b4",
+            "#73ac31"
+        ]
+    },
+
+
+    // Charizard
+    {
+        types: ["Fire", "Flying"],
+        colors: [
+            "#f97e32",
+            "#f6d55c",
+            "#4b6cb7"
+        ]
+    },
+
+
+    // Blastoise
+    {
+        types: ["Water"],
+        colors: [
+            "#3b82c4",
+            "#d6e6f2",
+            "#6b8e23"
+        ]
+    },
+
+
+    // Pikachu
+    {
+        types: ["Electric"],
+        colors: [
+            "#f6c945",
+            "#8b5a2b",
+            "#2f2f2f"
+        ]
+    },
+
+
+    // Gengar
+    {
+        types: ["Ghost", "Poison"],
+        colors: [
+            "#705898",
+            "#3c2a4d",
+            "#f2d94e"
+        ]
+    },
+
+
+    // Eevee
+    {
+        types: ["Normal"],
+        colors: [
+            "#a86f43",
+            "#f5d6a1",
+            "#4b3024"
+        ]
+    },
+
+
+    // Vaporeon
+    {
+        types: ["Water"],
+        colors: [
+            "#4f86c6",
+            "#7ec8e3",
+            "#f1f1f1"
+        ]
+    },
+
+
+    // Jolteon
+    {
+        types: ["Electric"],
+        colors: [
+            "#f4d35e",
+            "#fff3b0",
+            "#9c6b30"
+        ]
+    },
+
+
+    // Umbreon
+    {
+        types: ["Dark"],
+        colors: [
+            "#252525",
+            "#e6c84f",
+            "#3f5f8f"
+        ]
+    },
+
+
+    // Espeon
+    {
+        types: ["Psychic"],
+        colors: [
+            "#d98cb3",
+            "#9b5de5",
+            "#f7cad0"
+        ]
+    },
+
+
+    // Scizor
+    {
+        types: ["Bug", "Steel"],
+        colors: [
+            "#c62828",
+            "#7b1e1e",
+            "#d9d9d9"
+        ]
+    },
+
+
+    // Gardevoir
+    {
+        types: ["Psychic", "Fairy"],
+        colors: [
+            "#8fd3c7",
+            "#f1f1f1",
+            "#d95f8a"
+        ]
+    },
+
+
+    // Lucario
+    {
+        types: ["Fighting", "Steel"],
+        colors: [
+            "#2d5f9a",
+            "#1d1d1d",
+            "#b9c7d9"
+        ]
+    },
+
+
+    // Milotic
+    {
+        types: ["Water"],
+        colors: [
+            "#8ed1fc",
+            "#e8c7ff",
+            "#d95f76"
+        ]
+    },
+
+
+    // Zoroark
+    {
+        types: ["Dark"],
+        colors: [
+            "#4a1f3d",
+            "#d93654",
+            "#8b5cf6"
+        ]
+    },
+
+
+    // Chandelure
+    {
+        types: ["Ghost", "Fire"],
+        colors: [
+            "#71368a",
+            "#ff7b00",
+            "#2b2b2b"
+        ]
+    },
+
+
+    // Dragapult
+    {
+        types: ["Dragon", "Ghost"],
+        colors: [
+            "#2f4b7c",
+            "#5f6caf",
+            "#8ac6d1"
+        ]
+    },
+
+
+    // Corviknight
+    {
+        types: ["Flying", "Steel"],
+        colors: [
+            "#242424",
+            "#4f4f4f",
+            "#8a8a8a"
+        ]
+    },
+
+
+    // Frosmoth
+    {
+        types: ["Ice", "Bug"],
+        colors: [
+            "#b8f2e6",
+            "#d8e2dc",
+            "#7a9cc6"
+        ]
+    },
+
+
+    // Gligar
+    {
+        types: ["Ground", "Flying"],
+        colors: [
+            "#de83b4",
+            "#ac6294",
+            "#4d74ba"
+        ]
+    }
+
+];
